e27cf7a8c30a3849b51054916e92ce7855a52ca7
Anil Mathews <anil@mathews.com>
2026-01-24 11:26:53 +0100

    update route.ts

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export const GET = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://www.anilmathews.com/</loc></url>
   <url><loc>https://www.anilmathews.com/about-anil-mathews</loc></url>
+  <url><loc>https://www.anilmathews.com/about-anil-mathews/fact-sheet</loc></url>
   <url><loc>https://www.anilmathews.com/press</loc></url>
   <url><loc>https://www.anilmathews.com/speaking</loc></url>
   <url><loc>https://www.anilmathews.com/ventures</loc></url>
@@ -16,6 +17,6 @@ export const GET = () => {
 </urlset>`;
 
   return new NextResponse(sitemap, {
-    headers: { 'Content-Type': 'application/xml' },
+    headers: { "Content-Type": "application/xml" },
   });
 };
